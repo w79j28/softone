cd0f0b4fa1bf2819e776bcef9744d07b1e1ce108
wjz <wjz@email.com>
2016-10-11 22:25:12 +0800

    del

--- src/com/wsoft/util/Demo.java
+++ src/com/wsoft/util/Demo.java
@@ -3,7 +3,6 @@ package com.wsoft.util;
 public class Demo {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.println("demo hello");
     }
 
